3324b1c8bfd58b019377fb9b4b49bc4620c3a77c
FoosN <nicolas.foos@synchrotron-soleil.fr>
2014-09-11 16:34:40 +0200

    First Documentation and anotation for the script

--- strategy_xscale.py
+++ strategy_xscale.py
@@ -10,7 +10,16 @@ import sys
 import re
 import time
 
-def StartingOpen():              
+def StartingOpen():   
+    """ Function open the initial XSCALE.INP file,
+        The easyest way to work is to use standard XSACLE.INP correctly setup 
+        with the default parameters you want (output, input). On way is to use
+        xscale2.py from Pierre Legrand
+        This function return xdsinp which is list containing all the XSCALE.INP 
+        lines.     
+    """
+# arg is global, because it's re-used during code execution as parameters to
+# find the relativ-path.           
     global arg
     for arg in sys.argv[1:]:
         try:
@@ -32,8 +41,10 @@ def StartingOpen():
             exit(0)       
 
 def makeResultFile(create):
-    if create :
-        #global resultFile        
+    """ Function create folder to write the file generate by other function
+        Default name is given to avoid conflict or error.    
+    """    
+    if create :        
         resultFile = raw_input ('give name for result file, default is : result.' \
 +str(time.time())+ ' :')
         if resultFile == '' :
@@ -50,12 +61,20 @@ def symlink_creation(srcFile, dest):
     os.symlink(srcFile, dest)   
 
 def writing_list_in_file(path, file2write):
+    """ Function to write string contain in a list 
+    """    
     outputfile = open(os.path.join(path, "XSCALE.INP"), 'a')
     for line in file2write:    
         outputfile.write(line)
         
 #function to edit the file path    
 def editFilePath(xdsinp):
+    """ Function to modify the path write in original XSCALE.INP, 
+        the new path write in the new XSCALE.INP create, will be set according 
+        the right path to read the symlink create by the symlink creation 
+        function. The old path is change in 0,1,2... .HKL corrsponding to 
+        the symlink 
+    """
     numberOfFile = []
     for lines in xdsinp:
         if re.findall(r"INPUT_FILE=", lines):
@@ -73,7 +92,6 @@ def changeFriedelsettings(xdsinp, settings):
                 xdsinp.__setitem__(xdsinp.index(lines), "      FRIEDEL'S_LAW= TRUE\n")
 
 def settings_XDS_strictAbsCorr(xdsinp, settings):
-  ### strict abs correction : 
     if "-sa" in settings:
         for lines in xdsinp:
             if re.findall(r"STRICT_ABSORPTION_CORRECTION=", lines):
@@ -130,7 +148,8 @@ def resolutionMax(xdsinp):
             if re.findall(r"INCLUDE_RESOLUTION_RANGE=", lines):               
                 xdsinp.__setitem__(xdsinp.index(lines), "INCLUDE_RESOLUTION_RANGE= 50.000    "+str(resMax)+"\n")    
     except : 
-        
+        #except is using to take in count the case where the user doesn't 
+        # want to modify the resolutuion parameter
         for lines in xdsinp:
             if re.findall(r"INCLUDE_RESOLUTION_RANGE=", lines):
                 resMax = float(lines[-10:])  
@@ -156,8 +175,8 @@ def resolutionShells(xdsinp):
             
     
 
-
-
+# this defined 5 schemes for automatic usage of this script in order to 
+# test different strategy of scaling with XSCALE
 scheme0 = ["-corr","-ano"]
 scheme1 = ["-sa", "-corr", "-ano"] 
 scheme2 = ["-sa","-corr", "-noAno"]
@@ -168,7 +187,6 @@ scheme5 = ["-zd", "-sa", "-corr", "-noAno"]
  
 listOfexperiment = [scheme0, scheme1, scheme2, scheme3, scheme4, scheme5]
 listOfFile = []             
-print listOfexperiment  #debug
 #open file and create output directory
 create = StartingOpen()
 resultFile = makeResultFile(create)
@@ -177,17 +195,19 @@ resolutionMax(xdsinp)
 for lines in xdsinp:
     if re.findall(r"INPUT_FILE=", lines):
         listOfFile.append(lines)
-# create link which point to the original XDS_ASCII.HKL files
 for i in listOfFile:
+    #this line correspond to the need of only one parts of the original path
+    # initially typed by the user. It's concatenation of the strating parts of
+    # the original path typed and the current path to the symlink newly created
+    # just before.
     link = symlink_creation(arg[:-10]+i[15:-1], "./"+str(resultFile)+"/"+str(listOfFile.index(i))+".HKL")
 listofFolder = []
 for scheme in listOfexperiment:
-    print scheme
+    # create differents folders for each scheme used
     listofFolder.append(resultFile+"/"+"scheme"+ str(listOfexperiment.index(scheme)))
-print str(listofFolder)+"list of folder"   #debug
-
 for path in listofFolder:
     os.mkdir(path)   
+
 file2write = []
 for scheme in listOfexperiment:
     settings = scheme
@@ -195,6 +215,8 @@ for scheme in listOfexperiment:
     resolutionShells(xdsinp)
     changeFriedelsettings(xdsinp, settings)
     merge(xdsinp, settings)
+# the line just after remove the CORRECTION, CRYSTAL_NAME, STRICT_ABSORBTION
+# already write by the precedent round of the loop    
     for line in xdsinp:    
         if re.findall(r"CORRECTIONS=", line):        
             xdsinp.pop(xdsinp.index(line))
@@ -208,6 +230,8 @@ for scheme in listOfexperiment:
             xdsinp.pop(xdsinp.index(line))     
     settings_XDS_strictAbsCorr(xdsinp, settings)
     i = listOfexperiment.index(scheme)
+# writing XSCALE.INP at each round of the loop, because xdsinp is modify 
+# iterativelly and that caused the loose of your parameters
     pathXscaleInp = str(listofFolder[i])+"/"
     writing_list_in_file(pathXscaleInp, xdsinp)
 
